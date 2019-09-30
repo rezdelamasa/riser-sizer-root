1dc70646fd3d62d37dfadb979e944291ae428111
Rez Delamasa <rezdelamasa@gmail.com>
2019-09-29 22:09:55 -0500

    Use environment variables in our functions

--- create.js
+++ create.js
@@ -5,7 +5,7 @@ import { success, failure } from "./libs/response-lib";
 export async function main(event, context) {
   const data = JSON.parse(event.body);
   const params = {
-    TableName: "riser-sizer-user-properties",
+    TableName: process.env.tableName,
     Item: {
       userId: event.requestContext.identity.cognitoIdentityId,
       // projectId: uuid.v1(),
