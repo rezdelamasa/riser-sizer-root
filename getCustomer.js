e68443580d71a9b4775b7a38bba05fe2fc7804c0
Rez Delamasa <rezdelamasa@gmail.com>
2019-10-17 22:50:17 -0500

    Define customerId as event.body as string

--- getCustomer.js
+++ getCustomer.js
@@ -2,7 +2,7 @@
 import { success, failure } from "./libs/response-lib";
 
 export async function main(event, context) {
-  const customerId = event.body;
+  const customerId = event.body + '';
   // const amount = 1500;
   // const description = "Riser Sizer Monthly Subscription - Basic";
   // Load our secret key from the  environment variables
@@ -11,7 +11,7 @@ export async function main(event, context) {
   // Set your secret key: remember to change this to your live secret key in production
   // See your keys here: https://dashboard.stripe.com/account/apikeys
   let customerObject = await stripe.customers.retrieve(
-    event.body
+    customerId
   );
 
   // let flag = false;
