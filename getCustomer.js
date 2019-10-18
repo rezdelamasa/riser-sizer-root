38faf597c5eac2cb6d82a7d9f183af63d1c243d2
Rez Delamasa <rezdelamasa@gmail.com>
2019-10-17 22:56:29 -0500

    Undefine customerId

--- getCustomer.js
+++ getCustomer.js
@@ -2,7 +2,7 @@
 import { success, failure } from "./libs/response-lib";
 
 export async function main(event, context) {
-  const customerId = event.body + '';
+  // const customerId = event.body + '';
   // const amount = 1500;
   // const description = "Riser Sizer Monthly Subscription - Basic";
   // Load our secret key from the  environment variables
