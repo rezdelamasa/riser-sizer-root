5700ee4fef09c1bdf52c3138a07d3316426e70af
Rez Delamasa <rezdelamasa@gmail.com>
2019-10-17 20:56:12 -0500

    Redefine stripe

--- getCustomer.js
+++ getCustomer.js
@@ -1,11 +1,11 @@
-import stripePackage from "stripe";
+// import stripePackage from "stripe";
 import { success, failure } from "./libs/response-lib";
 
 export async function main(event, context) {
   // const amount = 1500;
   // const description = "Riser Sizer Monthly Subscription - Basic";
   // Load our secret key from the  environment variables
-  const stripe = stripePackage(process.env.stripeSecretKey);
+  const stripe = require('stripe')('sk_test_KFZXlDCk4Yazdjn7fjkAJHaA00QlhtfFlX');
 
   // Set your secret key: remember to change this to your live secret key in production
   // See your keys here: https://dashboard.stripe.com/account/apikeys
