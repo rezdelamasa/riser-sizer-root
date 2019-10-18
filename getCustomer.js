39dc9db21e207bf937dae3d06d50a8385c3d979b
Rez Delamasa <rezdelamasa@gmail.com>
2019-10-17 23:22:10 -0500

    Fix variable name

--- getCustomer.js
+++ getCustomer.js
@@ -3,7 +3,7 @@ import { success, failure } from "./libs/response-lib";
 
 export async function main(event, context) {
   // const customerId = event.body + '';
-  const { data } = JSON.parse(event.body);
+  const { body } = JSON.parse(event);
   // const amount = 1500;
   // const description = "Riser Sizer Monthly Subscription - Basic";
   // Load our secret key from the  environment variables
@@ -24,7 +24,7 @@ export async function main(event, context) {
   // let message = (flag) ? "Subscribed" : "Not subscribed";
 
   try {
-    return success(data);
+    return success(body);
   } catch (e) {
     return failure({ message: e.message });
   }
