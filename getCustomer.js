141394c065e330bf737bf94712c599b0030924dd
Rez Delamasa <rezdelamasa@gmail.com>
2019-10-17 20:52:18 -0500

    Define stripe

--- getCustomer.js
+++ getCustomer.js
@@ -5,7 +5,7 @@ export async function main(event, context) {
   // const amount = 1500;
   // const description = "Riser Sizer Monthly Subscription - Basic";
   // Load our secret key from the  environment variables
-  // const stripe = stripePackage(process.env.stripeSecretKey);
+  const stripe = stripePackage(process.env.stripeSecretKey);
 
   // Set your secret key: remember to change this to your live secret key in production
   // See your keys here: https://dashboard.stripe.com/account/apikeys
