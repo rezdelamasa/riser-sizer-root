223c871dab8fb3659268fe0b1904353bac996936
Rez Delamasa <rezdelamasa@gmail.com>
2019-10-23 14:12:35 -0500

    Remove db definition

--- billing.js
+++ billing.js
@@ -1,5 +1,5 @@
 // import stripePackage from "stripe";
-import * as dynamoDbLib from "./libs/dynamodb-lib";
+// import * as dynamoDbLib from "./libs/dynamodb-lib";
 import { success, failure } from "./libs/response-lib";
 
 export async function main(event, context) {
